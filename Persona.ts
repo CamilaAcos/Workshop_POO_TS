3784818bd3b50d186c3d63c983bd5911539ef6d9
CamilaAcos <camiacofer@gmail.com>
2024-09-25 10:27:57 -0500

    hola

---
+++ Persona.ts
@@ -0,0 +1,13 @@
+class Persona {
+name: string
+age: number
+
+constructor(name:string, age:number){
+    this.name = name
+    this.age = age   
+
+}
+saludar():void {
+console.log(`Persona: Tu nombre es ${this.name} y tienes ${this.age} años`)
+}
+}
