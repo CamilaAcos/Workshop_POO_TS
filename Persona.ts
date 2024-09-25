f97ddd07347be5413d5125f9ccf673e0c883c547
CamilaAcos <camiacofer@gmail.com>
2024-09-25 10:55:57 -0500

    ejercicio nro 02

--- Persona.ts
+++ Persona.ts
@@ -1,4 +1,4 @@
-class Persona {
+export class Persona {
 name: string
 age: number
 
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Persona } from "./Persona"
+
 const Persona1 = new Persona ("Jose", 52)
 const Persona2 = new Persona ("Laura", 25)
 const Persona3 = new Persona ("Andrea", 29)
@@ -8,3 +10,29 @@ const Persona7 = new Persona ("Valeria", 27)
 const Persona8 = new Persona ("Santiago", 39)
 const Persona9 = new Persona ("Samuel", 32)
 const Persona10 = new Persona ("Sara", 40)
+
+Persona1.saludar()
+Persona2.saludar()
+Persona3.saludar()
+Persona4.saludar()
+Persona5.saludar()
+Persona6.saludar()
+Persona7.saludar()
+Persona8.saludar()
+Persona9.saludar()
+Persona10.saludar()
+
+/*const personas = [
+    new Persona("Jose", 52),
+    new Persona("Laura", 25),
+    new Persona("Andrea", 29),
+    new Persona("Alejandro", 30),
+    new Persona("Eliana", 35),
+    new Persona("Julio", 33),
+    new Persona("Valeria", 27),
+    new Persona("Santiago", 39),
+    new Persona("Samuel", 32),
+    new Persona("Sara", 40),
+  ]
+  
+  personas.forEach(persona => persona.saludar())*/
