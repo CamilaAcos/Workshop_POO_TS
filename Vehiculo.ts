f0c4131c011cf561fa89a938d24576d35570a61d
CamilaAcos <camiacofer@gmail.com>
2024-09-30 19:43:55 -0500

    Ejercicio nro 10

---
+++ Vehiculo.ts
@@ -0,0 +1,26 @@
+abstract class Vehiculo {
+
+    constructor() { }
+
+    abstract arrancar(): void;
+
+}
+
+class Coche extends Vehiculo {
+    constructor() {
+        super();
+    }
+    arrancar(): void {
+        console.log("Arrancando el coche")
+    }
+}
+
+class Moto extends Vehiculo {
+    constructor() {
+        super();
+    }
+    arrancar(): void {
+        console.log("Arrancando la moto")
+    }
+
+}
